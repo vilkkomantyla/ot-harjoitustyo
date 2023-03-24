8b14a226db25c384946cc661bef46e839e2937c8
Ville-Veikko Mäntylä <villevem@helsinki.fi>
2023-03-24 12:04:59 +0200

    testit ja coverage

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,22 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+    
+    def test_kortin_saldo_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+    
+    def test_rahan_lataaminen_kasvattaa_arvoa(self):
+        self.maksukortti.lataa_rahaa(1000)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 20.00 euroa")
+    
+    def test_rahan_ottaminen_toimii_jos_rahaa_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+    
+    def test_saldo_ei_muutu_jos_rahaa_ei_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(1500)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+    
+    def test_metodi_palauttaa_oikean_totuusarvon(self):
+        self.maksukortti.ota_rahaa(500) == True
+        self.maksukortti.ota_rahaa(1500) == False
